a80623d9e3bd1b75bfe1121b826457859935248e
SteveDexter24 <40054683+SteveDexter24@users.noreply.github.com>
2020-02-13 18:22:46 +0800

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 // Fade in of the page contents
 $("html").hide();
-$("html").fadeIn(1200);
+$("html").fadeIn(2000);
 
 // "Special" link bar to hide or show an extra bar of buttons for 4 task
 $(document).ready(function () {
@@ -8,8 +8,16 @@ $(document).ready(function () {
   $('.navbar-toggler').click(function() {
     $('#navbarNavDropdown').toggleClass("collapse", "dropdown");
   });
-  // Click to toggle Special
+  // close navbar hamburger menu when item in the navbar is clicked (for mobile devices only)
+  $(".nav-item-1").click(function() {
+    $('#navbarNavDropdown').toggleClass("collapse", "dropdown");
+  });
+  // Show and close bottom navbar when "special" is clicked
   $("#special").click(function() {
-    $('#special-bar').removeClass(["sp-bar"]);
+    $('#special-bar').toggleClass("sp-bar", "sp-bar");
+  });
+  // close bottom navbar when 'close' is clicked
+  $(".close-nav-bar").click(function() {
+    $('#special-bar').toggleClass("sp-bar", "sp-bar");
   });
 });
